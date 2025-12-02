2919b211a3c88a6c47e077df16b0d8d42265e7c0
medotnull <meenalbansodbmw@gmail.com>
2025-12-03 03:14:06 +0530

    without postgres

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+pub struct Db {
+
+}
+
+// prisma, deisel, sqlx (sqlx-migrate in later) for db operations 
+impl Db {
+    pub async fn create_user(&self) {
+        // Implementation goes here
+    }
+
+    pub async fn get_user_by_username(&self, username: String) {
+        // Implementation goes here
+    }
+
+    pub async fn get_user_by_id(&self, id: String) {
+        // Implementation goes here
+    }
+
+}
